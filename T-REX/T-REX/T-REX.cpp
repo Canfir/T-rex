2bc522b9e97a9e5859ce62f5f8d4c22d7b7032b0
Canfir <66051249+Canfir@users.noreply.github.com>
2020-05-28 19:39:36 +0700

    Main+Ch

--- T-REX/T-REX/T-REX.cpp
+++ T-REX/T-REX/T-REX.cpp
@@ -1,1 +1,51 @@
-﻿
+﻿#include <iostream>
+#include <conio.h>
+
+#pragma warning(disable:4996) // POSIX name deprecated
+
+void run();
+void initMap();
+void changeDirection(char key);
+void update();
+void clearScreen();
+void printMap();
+
+bool running;
+
+int direction;
+
+int main()
+{
+	run();
+	return 0;
+}
+
+void run()
+{
+	initMap();
+	running = true;
+	while (running) {
+		if (kbhit()) {
+			changeDirection(getch());
+		}
+		update();
+
+		clearScreen();
+
+		printMap();
+
+		_sleep(500);
+	}
+
+	std::cout << "\t\t!!!Game over!" << std::endl << "\t\tYour score is: " << food;
+
+	std::cin.ignore();
+}
+
+void changeDirection(char key) {
+	switch (key) {
+	case 'w':
+		direction = 0;
+		break;
+	}
+}
