657211a8ca97a18da66efe9f8975ae05ce6b132c
Alex-salk <66050285+Alex-salk@users.noreply.github.com>
2020-05-28 19:47:01 +0700

    guhi

--- T-REX/T-REX/T-REX.cpp
+++ T-REX/T-REX/T-REX.cpp
@@ -13,7 +13,10 @@ void printMap();
 bool running;
 
 int direction;
-
+const int mapwidth = 20;
+const int mapheight = 20;
+const int size = mapwidth * mapheight;
+int map[size];
 int main()
 {
 	run();
@@ -48,4 +51,30 @@ void changeDirection(char key) {
 		direction = 0;
 		break;
 	}
+}
+char getMapValue(int value)
+{
+	// Returns a part of snake body
+	if (value > 0) return 'o';
+
+	switch (value) {
+		// Return wall
+	case -1: return 'X';
+		// Return food
+	case -2: return 'O';
+	}
+
+	return ' ';
+}
+void printMap()
+{
+	for (int x = 0; x < mapwidth; ++x) {
+		for (int y = 0; y < mapheight; ++y) {
+			// Prints the value at current x,y location
+			std::cout << getMapValue(map[x + y * mapwidth]);
+		}
+		// Ends the line for next x value
+		std::cout << std::endl;
+	}
+	std::cout << "Vas schet:" << s;
 }
